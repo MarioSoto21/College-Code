443badb0ebc665dccd7360434f6c77ed1d015c9a
MarioSoto <111585938+MarioSoto21@users.noreply.github.com>
2025-04-29 20:35:05 -0600

    Update

--- CS 458 - Mobile Application Devopment/Secure-Shelled-Messenger/SERVER/server.api/src/main/java/com/ssm/server/api/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssm.server.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ssm.server.api.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
